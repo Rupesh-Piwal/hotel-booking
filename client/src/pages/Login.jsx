b5c472b0203511abd331a8e80e2c297427515f59
rupesh <rpiwal02@gmail.com>
2025-03-06 09:10:08 +0530

    minor changes

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -69,19 +69,6 @@ const Login = () => {
             </div>
 
             <div className="flex items-center justify-between mb-6">
-              <div className="flex items-center">
-                <input
-                  id="remember-me"
-                  type="checkbox"
-                  className="h-4 w-4 text-blue-600 focus:ring-blue-500 border-gray-300 rounded"
-                />
-                <label
-                  htmlFor="remember-me"
-                  className="ml-2 block text-sm text-gray-700"
-                >
-                  Remember me
-                </label>
-              </div>
               <div className="text-sm">
                 <a
                   href="#"
